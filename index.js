34f8125c15bfcfb99780f150690530b4e3c168d9
codic-yeeshu <yeeshu.exploresmore@gmail.com>
2024-12-12 00:57:39 +0530

    updated index js with required tasks

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 const express = require("express");
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
 const app = express();
 const port = 3000;
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-dotenv.config()
+dotenv.config();
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+async function getResponse(prompt) {
+  const API_KEY = "AIzaSyDe8fDZPoSjPgSye8s-DWpi7GpB0njFVao";
+  const genAI = new GoogleGenerativeAI(API_KEY);
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+  const result = await model.generateContent(prompt);
+  return result.response.text();
+}
+
+app.post("/api/gemini/prompt/send", async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    if (!prompt || prompt == "")
+      return res.status(400).json({
+        message: "Please send a valid prompt",
+      });
+
+    const aiResponse = await getResponse(prompt);
+    return res.status(200).json({
+      response: `${aiResponse}`,
+    });
+  } catch (err) {
+    console.error(`Error occurred in file: index.js, function: post -`, err);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
 });
 
-module.exports = {app}
+module.exports = { app };
